a1900aa20ddf5ada3ffacc98b13bce280dc1795c
MWiedmeier <mattwiedmeier@gmail.com>
2015-04-28 13:36:10 -0400

    Up to date

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -8,19 +8,26 @@ public class EnemyAI : MonoBehaviour {
 	public float chaseWaitTime = 5f;
 	public float patrolWaitTime = 1f;
 	public Transform[] wayPoints;
+	public float attackCoolDown = 2.0f;
+	public int damage = 10;
+
 
 	private EnemySight enemySight;
 	private NavMeshAgent agent;
 	private Transform player;
 	private PlayerHealth playerHealth;
+	private GoblinAttack goblinAttack;
 	private float chaseTimer;
 	private float patrolTimer;
+	private float attackTimer;
 	private int wayPointIdex;
 	private Animator anim;
 	private int point = 0;
+
 	void Awake(){
 
 		enemySight = GetComponent<EnemySight> ();
+		goblinAttack = GetComponentInChildren<GoblinAttack> ();
 		agent = GetComponent<NavMeshAgent> ();
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
 		playerHealth = player.GetComponent<PlayerHealth> ();
@@ -32,9 +39,14 @@ public class EnemyAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		if (enemySight.playerInSight && playerHealth.currentHealth > 0f && 1==2) {
+		attackTimer += Time.deltaTime;
+		if (enemySight.playerInSight && playerHealth.currentHealth > 0f && goblinAttack.playerInRange && attackTimer > attackCoolDown) {
+
 			// Attack the player
-			//Debug.Log ("1st If");
+			Debug.Log("Attacking!");
+			Attack();
+
+
 
 		}
 		else if(enemySight.personalLastSighting != enemySight.resetPosition && playerHealth.currentHealth > 0f){ 
@@ -45,7 +57,7 @@ public class EnemyAI : MonoBehaviour {
 		}
 		else{ 
 			// Follow waypoints
-			anim.SetFloat("Speed",2.1f);
+			anim.SetFloat("Speed",1.1f);
 			//Debug.Log("Waypoints")
 			FollowRoute();
 		}
@@ -54,16 +66,16 @@ public class EnemyAI : MonoBehaviour {
 
 	void Chasing(){
 
-		anim.SetFloat ("Speed", 1.1f);
+		anim.SetFloat ("Speed", 2.1f);
 		Vector3 sightingDeltaPos = enemySight.personalLastSighting - transform.position;
 
-		Debug.Log ("SQR:" + sightingDeltaPos.sqrMagnitude);
+		//Debug.Log ("SQR:" + sightingDeltaPos.sqrMagnitude);
 		if (sightingDeltaPos.sqrMagnitude > 4f) {
 
 			agent.destination = enemySight.personalLastSighting;
 		}
 
-		Debug.Log ("Remaining" + agent.remainingDistance);
+		//Debug.Log ("Remaining" + agent.remainingDistance);
 		if (agent.remainingDistance < agent.stoppingDistance) {
 
 			anim.SetFloat("Speed",0f);
@@ -97,4 +109,13 @@ public class EnemyAI : MonoBehaviour {
 		}
 
 	}
+
+	void Attack(){
+
+		attackTimer = 0.0f;
+		anim.SetTrigger ("Attack");
+		playerHealth.TakeDamage (damage);
+
+	}
 }
+
--- Assets/Scripts/Enemy/GoblinAttack.cs
+++ Assets/Scripts/Enemy/GoblinAttack.cs
@@ -2,62 +2,31 @@
 using System.Collections;
 
 public class GoblinAttack : MonoBehaviour {
-
-	public int damage = 10;
-	public float attackCoolDown = 3f;
-
-	Animator anim;
+	
+	public bool playerInRange;
 	GameObject player;
-	PlayerHealth playerHealth;
-	GoblinHealth goblinHealth;
-	bool playerInRange;
-	float timer;
 
 	// Use this for initialization
 	void Awake () {
 	
 		player = GameObject.FindGameObjectWithTag ("Player");
-		playerHealth = player.GetComponent<PlayerHealth> ();
-		goblinHealth = GetComponentInParent<GoblinHealth> ();
-		anim = GetComponentInParent<Animator> ();
 
 	}
 
 	void OnTriggerEnter(Collider other){
 
 		if (other.gameObject == player) {
-			Debug.Log ("In Attack Range");
+			Debug.Log("Player In Attack Range");
 			playerInRange = true;
 		}
 	}
 
 	void OnTriggerExit(Collider other){
 		if (other.gameObject == player) {
+			Debug.Log("Player Leaving Attack Range");
+
 			playerInRange = false;
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		timer += Time.deltaTime;
-
-		if (timer >= attackCoolDown && playerInRange && goblinHealth.currentHealth > 0) {
-			Attack();
-		}
-	
-	}
-
-	void Attack(){
-
-		timer = 0f;
-
-		anim.SetTrigger ("Attack");
-
-		if(playerHealth.currentHealth > 0){
-			playerHealth.TakeDamage(damage);
-		}
-	}
-
 
 }
